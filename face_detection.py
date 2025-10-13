645a25b73ad18e712e41ae7e96a05267d739378e
lemlime <limola5336@gmail.com>
2025-10-14 07:47:17 +0800

    added the segment on face detection

---
+++ face_detection.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+#detecting faces in opencv using Haarcascade
+#face detection is performed using classifiers.
+#classifiers are algorithms which decide whether a given image is positive or negative
+#classifiers are typically trained on thousands of images, opencv comes with pre-trained classifiers
+#the 2 main ones are:
+#1. Haarcascades
+#2. Local Binary Patterns (advanced Haarcascades, not as prone to noise)
+
